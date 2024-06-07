825a1707cb9eed09cbff9ff9b70d6ce0f9ed6343
vitaliy <vit2001g@gmail.com>
2024-06-07 16:36:46 +0300

    Added class and constructor

---
+++ src/Task_03_2/BinTreeNode.java
@@ -0,0 +1,14 @@
+package Task_03_2;
+
+public class BinTreeNode<Object> {
+	public Object value;
+	public Object key;
+	BinTreeNode<Object> left, right;
+
+	BinTreeNode(Object key, Object value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	// TODO: Реализовать поиск
+}
---
+++ src/Task_03_2/Main.java
@@ -0,0 +1,11 @@
+/*
+Реализовать бинарное дерево поиска.
+ */
+
+package Task_03_2;
+
+public class Main {
+	public static void main(String args[]) {
+
+	}
+}
