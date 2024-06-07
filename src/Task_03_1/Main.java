d3235959f18daf20a18ef420699bace98e799298
vitaliy <vit2001g@gmail.com>
2024-06-07 16:14:10 +0300

    Task 03.1 done

--- src/Task_03_1/Main.java
+++ src/Task_03_1/Main.java
@@ -19,32 +19,36 @@ public class Main {
 
 	// Метод деления отрезка пополам
 	public static double halfSegment(double left, double right, double accuracy) {
-		// Проверка на разные знаки значений функций в left и right
-		if (Math.signum((float) f(left)) != Math.signum((float) f(right))) {
-			System.out.printf("Signs are not equal. Continue...\n");	// Знаки разные, продолжаем
+
+		if (Math.signum((float) f(left)) != Math.signum((float) f(right))) {				// Проверка на разные знаки значений функций в left и right
+			System.out.printf("Signs are not equal. Continue...\n");						// Знаки разные, продолжаем
 		} else {
 			System.out.printf("Signs are equal. Another borders of segment are required.");	// Знаки одинаковые, ошибка
-			// TODO: Добавить возврат ошибки
 			exit(0);
 		}
 
-		double segmentLength = right - left;	// Длина отрезка
+		double x = 0;
 		double center = 0;						// Центр отрезка
 
 		// Производим вычисления до определенной точности
-		while (segmentLength > accuracy) {
-			segmentLength /= 2;				// Длина новых отрезков
-			center = left + segmentLength;	// Новый центр отрезка
-
-			// TODO: Проанализировать значение функции в центре
+		while (Math.abs(right - left) > accuracy) {
+			center = (left + right) / 2;	// Новый центр отрезка
+			if (f(left) * f(center) <= 0) right = center;
+			else {
+				left = center;
+				x = (left + right) / 2;
+			}
 		}
+
+		return x;
 	}
 
 	public static void main(String args[]) {
 		int leftBorder = 0;		// Нижняя граница отрезка
 		int rightBorder = 10;	// Верхняя граница отрезка
 
-		// Вывод значений функции на границах отрезка
-		System.out.printf("f(%d) = %.2f\nf(%d) = %.2f\n", leftBorder, f(leftBorder), rightBorder, f(rightBorder));
+		// Вывод корня уравнения
+		double root = halfSegment(leftBorder, rightBorder, 0.001);
+		System.out.printf("Root: f(%.2f) = %.3f\n", root, f(root));
 	}
 }
