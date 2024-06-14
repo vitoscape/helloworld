bcd60002cdc392280b591b86ffb2e36e5bb90c3c
vitaliy <vit2001g@gmail.com>
2024-06-14 16:18:55 +0300

    Add insertNode() method

--- src/Task_03_2/BinTree.java
+++ src/Task_03_2/BinTree.java
@@ -23,4 +23,39 @@ public class BinTree<T extends Comparable<T>> {
 
 		return currentNode;
 	}
+
+	public void insertNode(T value) {
+		BinTreeNode<T> newNode = new BinTreeNode<>();
+		newNode.setValue(value);
+
+		if (rootNode == null) {							// If there is no root node in tree then new node is root node
+			rootNode = newNode;
+		} else {										// Root node is occupied
+			BinTreeNode<T> currentNode = rootNode;		// Start with root node
+			BinTreeNode<T> parentNode;
+
+			while(true) {
+				parentNode = currentNode;
+				if (value == currentNode.getValue()) {	// If there is same node in tree then return
+					return;
+
+				} else if (value.compareTo(currentNode.getValue()) < 0) {    // Move to left child
+					currentNode = currentNode.getLeftChild();
+
+					if (currentNode == null) {								// If the end of branch
+						parentNode.setLeftChild(newNode);					// Then insert to the left and return
+						return;
+					}
+
+				} else {													// Move to right child
+					currentNode = currentNode.getRightChild();
+
+					if (currentNode == null) {								// If the end of branch
+						parentNode.setRightChild(newNode);					// Then insert to the right and return
+						return;
+					}
+				}
+			}
+		}
+	}
 }
