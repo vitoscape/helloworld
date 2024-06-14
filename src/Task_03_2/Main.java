f043e397bf0025c462be286ec6e3fcc2271fb29d
vitaliy <vit2001g@gmail.com>
2024-06-14 16:02:46 +0300

    Test compareTo() method

--- src/Task_03_2/Main.java
+++ src/Task_03_2/Main.java
@@ -6,7 +6,16 @@ package Task_03_2;
 
 public class Main {
 	public static void main(String args[]) {
-		BinTreeNode<Integer> node = new BinTreeNode<>(19);
-		node.printNode();
+
+		// Integer test
+//		BinTreeNode<Integer> node1 = new BinTreeNode<>(21);
+//		BinTreeNode<Integer> node2 = new BinTreeNode<>(20);
+//		System.out.println(node1.compareTo(node2));
+
+		// String test
+		BinTreeNode<String> node1 = new BinTreeNode<>("9");
+		BinTreeNode<String> node2 = new BinTreeNode<>("!");
+		System.out.println(node1.compareTo(node2));
+		System.out.println(node1.toString() + node2.toString());
 	}
 }
