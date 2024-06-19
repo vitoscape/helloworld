22c23e2b99bd0ce4c043271d7d5f70f9fa5d1f5d
vitoscape <vit2001g@gmail.com>
2024-06-19 17:31:20 +0300

    Add example for using BinTree class

--- src/Task_03_2/Main.java
+++ src/Task_03_2/Main.java
@@ -7,15 +7,25 @@ package Task_03_2;
 public class Main {
 	public static void main(String args[]) {
 
-		// Integer test
-//		BinTreeNode<Integer> node1 = new BinTreeNode<>(21);
-//		BinTreeNode<Integer> node2 = new BinTreeNode<>(20);
-//		System.out.println(node1.compareTo(node2));
+		BinTree<Integer> tree = new BinTree<>();
+		tree.insertNode(6);
+		tree.insertNode(8);
+		tree.insertNode(5);
+		tree.insertNode(8);
+		tree.insertNode(2);
+		tree.insertNode(9);
+		tree.insertNode(7);
+		tree.insertNode(4);
+		tree.insertNode(10);
+		tree.insertNode(3);
+		tree.insertNode(1);
 
-		// String test
-//		BinTreeNode<String> node1 = new BinTreeNode<>("9");
-//		BinTreeNode<String> node2 = new BinTreeNode<>("!");
-//		System.out.println(node1.compareTo(node2));
-//		System.out.println(node1.toString() + node2.toString());
+		tree.printTree();
+
+		tree.deleteNode(5);
+		tree.printTree();
+
+		BinTreeNode<Integer> foundNode = tree.findNode(7);
+		foundNode.printNode();
 	}
 }
